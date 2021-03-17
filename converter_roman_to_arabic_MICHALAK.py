30cef0ca7d3582dde79a953e182c471e25a541b5
Szaszlyk <74797285+Szaszlyk@users.noreply.github.com>
2021-03-17 12:16:29 +0100

    wyjatek

--- converter_roman_to_arabic_MICHALAK.py
+++ converter_roman_to_arabic_MICHALAK.py
@@ -1,29 +1,35 @@
 def converter_roman_to_arabic(user_roman):
-    roman_numbers = 'IVXLCDM'   # deklaracja stringa liter odpowiadających rzymskim liczbom
-    arabic_numbers = [1, 5, 10, 50, 100, 500, 1000]     # deklaracja arabskich wartosci dla odpowiednich indeksow rzymskich znakow
-    user_arabic = []    # deklaracja ciagu do ktorego zostana zapisane arabksie odpowiedniki wprowadzonego rzmyskiego ciagu
-    user_arabic_final = []  # deklaracja ciagu do ktorego zostanie zapisana ostateczne arabska wersja ciagu uwzgledniajaca takie liczby jak np.: IV, IX, itd...
-    step_number = 0  # deklaracja kroku uzytego w funkcji if pozwalajacego na laczenie znakow: np.: IV w 4
-    user_roman_upper = user_roman.upper()   # zabezpieczenie przed wprowadzeniem malych liter. Zamiana wprowadzonego ciagu liter na ciag wielkich liter
+    roman_numbers = 'IVXLCDM'
+    arabic_numbers = [1, 5, 10, 50, 100, 500, 1000]
+    user_arabic = []
+    user_arabic_final = []
+    step_number = 0
+    user_roman_upper = user_roman.upper()
+
+    try:
+        for y in user_roman_upper:
+            assert(y in roman_numbers), "To nie jest rzymska liczba"
+    except:
+        raise
 
     for x in user_roman_upper:
-        user_arabic.append(arabic_numbers[roman_numbers.find(x)]) # przekonwertowanie ciagu liter na ciag odpowiadajacych im wartosci
+        user_arabic.append(arabic_numbers[roman_numbers.find(x)])
 
     while step_number < len(user_arabic):
-        if step_number < len(user_roman_upper):     # jezeli podany przez uzytkowanika ciag ma tylko jedna litere
+        if step_number < len(user_roman_upper):
             if len(user_arabic) == 1:
                 user_arabic_final.append(user_arabic[step_number])
                 break
             elif step_number < (len(user_arabic)-1) and user_arabic[step_number] < user_arabic[step_number+1]:
                 user_arabic_final.append(user_arabic[step_number + 1] - user_arabic[step_number])
-                print(user_arabic_final)
+                #print(user_arabic_final)
                 step_number = step_number + 2
-                print(step_number)
+                #print(step_number)
             else:
                 user_arabic_final.append(user_arabic[step_number])
-                print(user_arabic_final)
+                #print(user_arabic_final)
                 step_number = step_number + 1
-                print(step_number)
+                #print(step_number)
     print(f'Rzymskie: {user_roman_upper} to arabksie: {sum(user_arabic_final)}')
 
 
--- test.py
+++ test.py
@@ -1,4 +1,9 @@
-a = (10, 20, 30)
-print(a)
-print( a[1] ) # 20
-print( a[0] ) # 10
+try:
+    x = 1
+    y = 0
+    assert y != 0, "Invalid Operation"
+    print(x / y)
+
+# the errror_message provided by the user gets printed  
+except AssertionError as msg:
+    print(msg) 
